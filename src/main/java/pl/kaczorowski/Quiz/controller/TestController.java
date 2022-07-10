b2735c4cbc9901352a10ac1b7fd3a2f89d1f827d
Mateusz <kaczorowski91@gmail.com>
2022-07-10 20:12:58 +0200

    WIP 1.02

--- src/main/java/pl/kaczorowski/Quiz/controller/TestController.java
+++ src/main/java/pl/kaczorowski/Quiz/controller/TestController.java
@@ -2,15 +2,11 @@ package pl.kaczorowski.Quiz.controller;
 
 
 import lombok.AllArgsConstructor;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import pl.kaczorowski.Quiz.dto.QuestionDto;
 
-import java.util.List;
-
-@Controller
+@RestController
 @RequestMapping("/test" )
 @AllArgsConstructor
 public class TestController {
