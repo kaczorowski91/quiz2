7f86a960492c758e44bd5e95e505a428ebffef20
Mateusz <kaczorowski91@gmail.com>
2022-07-19 21:02:21 +0200

    WIP 1.03

--- src/main/java/pl/kaczorowski/Quiz/controller/UserAnswerController.java
+++ src/main/java/pl/kaczorowski/Quiz/controller/UserAnswerController.java
@@ -3,8 +3,7 @@ package pl.kaczorowski.Quiz.controller;
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 import lombok.AllArgsConstructor;
-
-import org.springframework.http.MediaType;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import pl.kaczorowski.Quiz.dto.UserAnswerDto;
 import pl.kaczorowski.Quiz.service.UserAnswerService;
@@ -13,19 +12,19 @@ import java.lang.reflect.Type;
 import java.util.ArrayList;
 
 @RestController
-@RequestMapping("/v1/")
+
 @AllArgsConstructor
+@RequestMapping("/v1/answer")
 public class UserAnswerController {
 
     private UserAnswerService userAnswerService;
 
-    @PostMapping(value = "useranswers",  consumes = MediaType.APPLICATION_JSON_VALUE)
     @CrossOrigin(origins = "*")
+    @RequestMapping( produces = "application/json", method = RequestMethod.POST)
     public String getAnswer(@RequestBody Object answerDtoList) {
 
         Gson gson = new Gson();
-        Type userAnswer = new TypeToken<ArrayList<UserAnswerDto>>() {
-        }.getType();
+        Type userAnswer = new TypeToken<ArrayList<UserAnswerDto>>() {}.getType();
         ArrayList<UserAnswerDto> userAnswerDtos = gson.fromJson(answerDtoList.toString(), userAnswer);
 
         return userAnswerService.checkAnswerList(userAnswerDtos);
